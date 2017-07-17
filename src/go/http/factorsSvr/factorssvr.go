dfde18a580529819076cc5c12b2c18610fe98a0f
Steven Dall <stvndall@gmail.com>
2017-07-17 21:11:30 +0200

    factorising http endpoint

---
+++ src/go/http/factorsSvr/factorssvr.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"strconv"
+	"github.com/stvndall/languagetechstats/src/go/services/factors"
+	"github.com/gorilla/mux"
+)
+func main(){
+	 router := mux.NewRouter().StrictSlash(true)
+	  router.HandleFunc("/{numbers}", factorise)
+		log.Fatal(http.ListenAndServe(":2500", router))
+}
+
+func factorise(w http.ResponseWriter, r *http.Request) {
+	number, err := strconv.Atoi(mux.Vars(r)["numbers"])
+	if(err != nil){
+		fmt.Fprintf(w,"%v",err)
+	}
+	
+	fmt.Fprintf(w, "%v",factors.PrimeFactors(number) )
+}
